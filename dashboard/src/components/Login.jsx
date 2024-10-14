90e92c19f7385986b1e4547fee1fbc9279afebaa
john paul oco <johnpauloco435@gmail.com>
2024-10-15 03:55:30 +0800

    test 2

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://core1.nodadogenhospital.com/api/v1/user/login",
+        "https://core1.nodadogenhospital.com/api/v1/user/login",
         { email, password, confirmPassword, role: "Admin" },
         {
           withCredentials: true,
